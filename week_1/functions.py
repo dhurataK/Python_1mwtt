c9a933fa5f83df472f03a8c675773a2c4e4b0b8b
Dhurata Kamberi <dhuratakamberi_@hotmail.com>
2018-08-02 21:01:17 +0200

    Pushed the functions.py file

---
+++ week_1/functions.py
@@ -0,0 +1,30 @@
+# Functions
+def add(a,b):
+    x = a + b
+    return x
+
+result = add(3,4)
+# print(result)
+
+# Tuples are like lists, the only difference is they are IMMUTABLE
+tuple_data = ('physics','math',2018,1995)
+print(tuple_data)
+
+#Dictionaries
+weekend = {"sun":"Sunday","mon":"Monday"}
+# print(weekend)
+# for key in weekend:
+#     print(weekend[key])
+#
+# weekend["fri"] ="Friday"
+# for key in weekend:
+#     print(weekend[key])
+# weekend["fri"] ="Thursday"
+# print(weekend)
+
+myList = ["1",9,"Hello"]
+capitals = {}
+capitals["pr"] = "Prishtina"
+capitals["tr"] = "Tirana"
+# for data in capitals:
+#     print(capitals[data])
